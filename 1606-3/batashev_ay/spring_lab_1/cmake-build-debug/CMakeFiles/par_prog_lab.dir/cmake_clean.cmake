46d5faa651989706b06b21c6db793adfeafe95c7
Alexander Batashev <alexbatashev@outlook.com>
2019-03-12 19:09:16 +0300

    Add lab 1

---
+++ 1606-3/batashev_ay/spring_lab_1/cmake-build-debug/CMakeFiles/par_prog_lab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/par_prog_lab.dir/main.cpp.o"
+  "par_prog_lab.pdb"
+  "par_prog_lab"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/par_prog_lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
